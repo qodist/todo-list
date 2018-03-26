46fb4f15225b09eae0595825ef894014c4d0189c
qodist <reyno75@gmail.com>
2018-03-26 18:40:34 +0300

    new save

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,23 @@
 //click off todo's
-$("li").on("click", function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
 //click on x to delete todo
-$("span").on("click", function(event){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+//listen for enter keypress
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //grabbing new user todo
+        var todoText = ($(this).val());
+        $(this).val("");
+        //create new ul and append li
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 });
